e0a50dd4a5240006a5828aa3f36d8c9c017d187f
Joao Vitor dos Santos <jvitorgit@gmail.com>
2026-04-16 17:51:41 -0300

    Initial commit - AWS project with Terraform

---
+++ infra/main.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.web_sg.name]
+
+    user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+
+              systemctl start httpd
+              systemctl enable httpd
+
+              echo "<h1>Projeto AWS com Terraform</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
